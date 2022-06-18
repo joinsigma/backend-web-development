f2a003040d17947942018d91e5b461f99fe086f5
rafapavi <pavi_e@hotmail.com>
2022-06-19 00:03:22 +0800

    Set up Capstone project module 1

---
+++ Modules/Mod1-Intro-to-Node/Lessons/lesson-functions.js
@@ -0,0 +1,16 @@
+/**
+ * function definitions
+ */
+
+function myNewFunction(var1, var2) {
+  console.log(var1, var2);
+}
+
+/**
+ * Arrow functions
+ */
+const addNumbers = (num1, num2) => {
+  return num1 + num2;
+};
+
+console.log(addNumbers(1, 2));
---
+++ Modules/Mod1-Intro-to-Node/Lessons/lesson-packages.js
@@ -0,0 +1,22 @@
+/**
+ * Using Lodash
+ *  - What is Lodash?
+ *    A modern JavaScript utility library delivering modularity, performance & extras.
+ *    https://lodash.com
+ *  - Why people use it?
+ *    It's a utility library full of functions that make it easier to manipulate arrays, objects and strings efficiently.
+ *  - Documentation
+ *    https://lodash.com/docs/4.17.15
+ */
+
+// Default javascript importing method
+const _ = require("lodash");
+
+// ES importing method
+//  import _lodash from "lodash";
+
+console.log(_.chunk([1, 2, 3, 4], 1));
+
+/**
+ * Using Inquirer.js
+ */
