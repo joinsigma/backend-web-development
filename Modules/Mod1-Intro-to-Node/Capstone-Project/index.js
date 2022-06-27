b93a9b1dce67df1a2774e4b3c807b91db8b127b2
rafapavi <pavi_e@hotmail.com>
2022-06-27 09:05:15 +0800

    Update Capstone project base

--- Modules/Mod1-Intro-to-Node/Capstone-Project/index.js
+++ Modules/Mod1-Intro-to-Node/Capstone-Project/index.js
@@ -4,10 +4,11 @@
  *  - Insertion sort
  *  - Bubble sort
  *  - Selection sort
+ *  - Merge sort
  *
  * For an additional challenge, try to write the functions for the sorting algorithms yourself!
  * Use inquirer to trigger the questions
- * 
+ *
  * Print out the `getTerminalOptions`, and once you select a sorting algo.
  * Trigger a question to enter a list as such:
  *  - 1, 2, 3, 4
@@ -15,42 +16,40 @@
  *  - 1; 2; 3; 4
  *  - 1 | 2 | 3 | 4
  * All these "options" should be sorted.
- * 
+ *
  * When sorting, print out the partially sorted list on every loop.
  * After the sorting is done, print out how long the sorting algorithm took for that list. And the print the `getTerminalOptions` again.
  */
 import inquirer from "inquirer";
 
-const triggerQuestion = async (dataType, question) => {
+const getTerminalOptions = async () => {
+  const options = {
+    "Insertion Sort": 1,
+    "Bubble Sort": 2,
+    "Selection Sort": 3,
+    "Merge Sort": 4,
+    Quit: 0,
+  };
   const answer = await inquirer.prompt({
-    type: dataType,
+    type: "rawlist",
     name: "data",
-    message: question,
+    message: "Select a sorting algorithm to test: ",
+    choices: [
+      "Insertion Sort",
+      "Bubble Sort",
+      "Selection Sort",
+      "Merge Sort",
+      "Quit",
+    ],
   });
-
-  return answer.data;
-};
-
-const getTerminalOptions = () => {
-  return `
-    Welcome to the sorting Algorithm tester
-
-    Select a sorting algorithm to test.
-    1. Insertion Sort
-    2. Bubble Sort
-    3. Selection Sort
-    0. Quit
-    `;
+  return options[answer.data];
 };
 
 async function main() {
+  // variable to store the user's option
   let option = -1;
-
-  while (option !== 0) {
-    console.log(getTerminalOptions());
-    option = await triggerQuestion("number", "What is your choice?\n");
-    console.log(option);
-  }
+  console.log("Welcome to the sorting Algorithm tester!\n");
+  option = getTerminalOptions();
 }
 
 await main();
