7773bb1274de4e9f10723233d627b2aac5d44291
rafapavi <pavi_e@hotmail.com>
2022-07-19 18:17:12 +0800

    Update Mod1 Assignment 1

--- Modules/Mod1-Intro-to-Node/Assignments/Assign-1-Debug-Prog/index.js
+++ Modules/Mod1-Intro-to-Node/Assignments/Assign-1-Debug-Prog/index.js
@@ -2,17 +2,18 @@
  * Complete `passTests`, so that all console logs will return true
  */
 function passTests() {
-  const var1 = "";
-  const var2 = "";
-  const var3 = "";
-  const var4 = "";
-  const var5 = "";
+  const var1 = null;
+  const var2 = null;
+  const var3 = null;
+  const var4 = null;
+  const var5 = null;
   console.log("var1 === 'eleven:'", var1 === "eleven");
   console.log("(var2 + var3) === 6", var2 + var3 === 6);
   console.log("(var2 * var3) === 8", var2 * var3 === 8);
+  console.log("Total sum of array 'var4' === 10", var4.reduce((p, c) => p + c) === 10);
   console.log(
-    "var4.concat(var5) === [1, 2, 3, 4]",
-    var4.concat(var5) === [1, 2, 3, 4]
+    "var4.concat(var5) === [1, 2, 3, 4, 5, 6, 7]",
+    var4.concat(var5).every((v) => [1, 2, 3, 4, 5, 6, 7].indexOf(v) >= 0)
   );
 }
 passTests();
@@ -29,6 +30,6 @@ function printTimeTaken() {
   `);
   // Calculate how long it took
   const howLong = "";
-  console.log(`I took this long to run: ${howLong}secs`); 
+  console.log(`I took this long to run: ${howLong}secs`);
 }
 printTimeTaken();
