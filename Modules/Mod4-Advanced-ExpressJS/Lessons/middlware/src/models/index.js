598258cd1b9ee63604b89cac261839444ef60b81
rafapavi <pavi_e@hotmail.com>
2023-03-23 02:16:56 +0800

    Mod4 - Add some lessons

---
+++ Modules/Mod4-Advanced-ExpressJS/Lessons/middlware/src/models/index.js
@@ -0,0 +1,37 @@
+import dbConfig from "../config/db.config.js";
+import Sequelize from "sequelize";
+
+// Models
+import CarModel from "./car.model.js";
+import CarModelModel from "./carModel.model.js";
+import UserModel from "./user.model.js";
+
+const db = {};
+
+export const startDbConnection = (hostname, username, password) => {
+  let sequelize = null;
+  if (!sequelize) {
+    sequelize = new Sequelize(dbConfig.DB, username, password, {
+      host: hostname,
+      dialect: dbConfig.dialect,
+    });
+    db.sequelize = sequelize;
+    db.car = CarModel(sequelize, Sequelize.DataTypes);
+    db.carModel = CarModelModel(sequelize, Sequelize.DataTypes);
+    db.user = UserModel(sequelize, Sequelize.DataTypes);
+
+    db.carModel.hasMany(db.car, {
+      foreignKey: "carModelId",
+    });
+    db.car.belongsTo(db.carModel, {
+      foreignKey: "carModelId",
+    });
+
+    db.sequelize.sync();
+  }
+  return db;
+};
+
+export const getConnection = () => {
+  return db;
+};
